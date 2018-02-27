16ac7898ee225795e210200da52db40cb6e50d50
Soyal <345723940@qq.com>
2018-02-27 16:01:49 +0800

    V2.1.2 (#12)
    * 补充上传失败的图片

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
   // 鱼说的图片域名
   imageDomain: 'image-cdn.fishsaying.com',
-  defaultImage: '//image-cdn.fishsaying.com/29b95c6cf1b04c9d9932093c6bd6544f.jpg@150w_150h'
+  defaultImage: '//image-cdn.fishsaying.com/29b95c6cf1b04c9d9932093c6bd6544f.jpg@150w_150h',
+  errorImage: '//static-cdn.fishsaying.com/cultrue-cc/upload_error.jpg'
 }
