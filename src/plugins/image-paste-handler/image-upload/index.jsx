60175b13ae8230c16e9064d1994eb00130c3a43a
Soyal <345723940@qq.com>
2018-03-01 22:14:16 +0800

    修复部分问题，但是draft mergeEntity的bug需要注意，有部分功能是基于此实现的

--- src/plugins/image-paste-handler/image-upload/index.jsx
+++ src/plugins/image-paste-handler/image-upload/index.jsx
@@ -5,7 +5,6 @@ import React, { Component } from 'react'
 import { isInDomain } from 'utils/common'
 import PropTypes from 'prop-types'
 import config from 'config.js'
-import { EditorState } from 'draft-js'
 
 class ImageHandler extends Component {
   static propTypes = {
@@ -23,13 +22,13 @@ class ImageHandler extends Component {
     super(props)
 
     this.state = this.getInitialState(props)
-    this.id = Math.random()
   }
 
   getInitialState(props) {
     const entityData = this.getData(props)
     const state = {
-      status: 'uploading' // uploading || uploaded
+      status: 'uploading', // uploading || uploaded
+      data: entityData
     }
 
     if (!this.shouldUpload(entityData)) {
@@ -46,7 +45,7 @@ class ImageHandler extends Component {
   shouldUpload(data) {
     const { src } = data
     if (src.startsWith('data:') || isInDomain(src)) return false
-    console.log('should upload: ', src)
+    // console.log('should upload: ', src)
     return true
   }
 
@@ -66,8 +65,7 @@ class ImageHandler extends Component {
    * 通过图片上传状态拼接template
    */
   getStatusTemplate() {
-    const { status } = this.state
-    const entityData = this.getData(this.props)
+    const { status, data } = this.state
     if (status === 'uploading') {
       return (
         <p style={{ color: '#c7c8c9', fontStyle: 'italic', fontSize: '14px' }}>
@@ -75,52 +73,35 @@ class ImageHandler extends Component {
         </p>
       )
     }
-
+    
     return (
       <figure contentEditable="false">
-        <img {...entityData} role="presentation" />
+        <img {...data} role="presentation" />
       </figure>
     )
   }
 
   updateEntity(data) {
-    const nContentState = this.props.contentState.mergeEntityData(
-      this.props.entityKey,
-      data
-    )
-    const editorState = EditorState.createWithContent(nContentState)
-    this.context.onChange(editorState)
-    // 替换entity data
-    // let nContentState = this.props.contentState.mergeEntityData(
-    //   this.props.entityKey,
-    //   data
-    // )
+    const cts = this.props.contentState
+    // it's so strange !!!
+    // why does this work!!!
+    // TODO
+    cts.mergeEntityData(this.props.entityKey, data)
+    this.context.onChange()
 
-    // // 设置selection
-    // const blockKey = this.getBlockKey(this.props.offsetKey)
-    // let nSelectionState = SelectionState.createEmpty(blockKey)
-    // // nContentState = Modifier.splitBlock(nContentState, nSelectionState)
-
-    // // 将entity apply到contentState上
-    // let nEditorState = EditorState.createWithContent(nContentState)
-    // nEditorState = EditorState.forceSelection(nEditorState, nSelectionState)
-    // nEditorState = AtomicBlockUtils.insertAtomicBlock(
-    //   nEditorState,
-    //   this.props.entityKey,
-    //   ' '
-    // )
-
-    // // const nEditorState = EditorState.createWithContent(nContentState)
-
-    // this.props.setEditorState(nEditorState)
+    const nData = Object.assign({}, this.state.data, data)
+    this.setState({
+      data: nData
+    })
   }
 
   async componentDidMount() {
+    // console.log('component did mount')
     if (this.state.status === 'uploading') {
-      console.log('do upload!!!', this.id)
       const entityData = this.getData(this.props)
       const { src } = entityData
       const data = await this.context.onImagePaste(src) // 处理完成的url
+
       // 上传图片成功
       let _targetSrc
       if (data.success) {
@@ -128,6 +109,10 @@ class ImageHandler extends Component {
       } else {
         _targetSrc = config.errorImage
       }
+
+      this.setState({
+        status: 'uploaded'
+      })
       this.updateEntity({ src: _targetSrc })
     }
   }
